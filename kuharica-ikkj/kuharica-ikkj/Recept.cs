3d57d2d1a438e45cd2bd3012cd273fd5a649b4ab
KJ HP <PC HP@PC>
2017-11-26 16:08:10 +0100

    added new class-recept

---
+++ kuharica-ikkj/kuharica-ikkj/Recept.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kuharica_ikkj
+{
+    class Recept
+    {
+        public string Name { get; set;}
+        public string Ingredient { get; set;}
+        public string PreparationTime { get; set;}
+        public string Course { get; set;}
+
+        public Recept (string name, string ingredient, string preptime, string course)
+        {
+            this.Name = name;
+            this.Ingredient = ingredient;
+            this.PreparationTime = preptime;
+            this.Course = course;
+        }
+
+        private void EmptyTextBox1 (string name, string ingredient, string preptime, string course)
+        {
+            name = string.Empty;
+            ingredient = string.Empty;
+            preptime = string.Empty;
+            course = string.Empty;
+        }
+
+
+    }
+}
